1e9ca768ddc9982e2b95ccda07e281bceccfa69a
Jorge Valhondo Rama <jrg.valhondo@gmail.com>
2019-08-06 20:35:12 +0100

    task_1: read dataset

--- project/task_1/task_1.py
+++ project/task_1/task_1.py
@@ -1,2 +1,10 @@
 # Playing with Spanish population dataset
+import os
 
+import pandas as pd
+
+print()
+
+df = pd.read_csv(os.path.join(os.path.dirname(__file__)) + '/data_population.csv')
+
+print(df.head())
